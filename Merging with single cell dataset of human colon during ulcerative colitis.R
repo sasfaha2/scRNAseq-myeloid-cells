5a85b252e17309377b19a6b609ac944e881861bd
Ojan Khosravifar <ojankhosravifar@gmail.com>
2022-09-30 17:15:53 -0400

    Update Merging with single cell dataset of human colon during ulcerative colitis.R

--- Merging with single cell dataset of human colon during ulcerative colitis.R
+++ Merging with single cell dataset of human colon during ulcerative colitis.R
@@ -8,7 +8,11 @@ library(reshape2)
 library(harmony)
 
 ### Human Data: Harmony Batch Correction
+# Visualize by patient prior to batch correction
+DimPlot(uc, reduction = 'pca', group.by = "patient")
 uc <- RunHarmony(uc, "patient", plot_convergence = TRUE)
+# Visualize by patient after to batch correction
+DimPlot(uc, reduction = 'harmony', group.by="patient")
 
 ### Human Data: Gene Ortholog Conversion
 mouse_homologs <- read.delim("../HOM_MouseHumanSequence.rpt.txt", header = TRUE, sep = "\t")
@@ -25,6 +29,24 @@ convert_mouse_to_human <- function(gene_list){
  }
  return (output)
 >}
+
+### Human and Mouse Merging
+# extract variable features for uc and mimmune
+var_genes_human <- VariableFeatures(uc)
+var_genes_mouse <- VariableFeatures(mimmune)
+var_genes <- unique(c(var_genes_human, var_genes_mouse))
+# add species to meta
+uc$species <- "human"
+mimmune$species <- "mouse"
+# remove t, b cells and neutros
+mimmune <- subset(mimmune, idents = c("Ly6cHi Monocytes", "Itgax- Macrophages", "Itgax+ Macrophages", "Resident Monocytes", "Alox15+ Macrophages"))
+# remove mast cells
+uc <- subset(uc, idents = c("Cd14Hi/Cd16Lo Macrophages", "Inflammatory Monocytes", "DC2", "Inflammatory Macrophages", "pre-DC2", "Nonclassical Monocytes", "Inflammatory Monocytes-2", "Cd14Hi/Cd16Lo Macrophages", "DC1", "pre-DC1", "Cycling Monocytes"))
+# remove non-variable genes
+uc <- uc[var_genes,]
+mimmune <- mimmune[var_genes,]
+# combine the two human and mouse objects
+hsmm <- merge(uc, mimmune)
 
 ### Human and Mouse Integration
 hsmm <- merge(uc, mimmune)
