df68451e25fefd899a08ad45a591ee71ed8101df
Ojan Khosravifar <ojankhosravifar@gmail.com>
2022-09-30 16:03:08 -0400

    Create Quality control and preprocessing.R

---
+++ Quality control and preprocessing.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(ggplot2)
+library(ggrepel)
+library(cluster)
+library(reshape2)
+
+# Quality Control, First Round (showing control only)
+control[["percent.mt"]] <- PercentageFeatureSet(control, pattern = "^mt-")
+# estimate 1.5*IQR for number of UMIs expressed; cells 1.5*IQR to be removed
+control.iqr <- as.vector(quantile(control$nFeature_RNA)[4] - quantile(control$nFeature_RNA)[2])
+control.upper <- as.vector((control.iqr * 1.5) + quantile(control$nFeature_RNA)[4])
+control <- subset(control, subset = nFeature_RNA 200 & nFeature_RNA < control.upper)
+# remove cells with 10% mito transcripts
+control <- subset(control, subset = percent.mt < 10)
+# remove essential 0s for genes (genes that are 0 across cells)
+control.zero <- which(!as.vector(rowSums(control) == 0))
+# merge control, dss and oxa into a single dataset
+myeloid <- merge(control, y = c(dss, oxa), add.cell.ids = c("control", "dss", "oxa"), project = "myeloid")
+
+# Preprocessing
+myeloid <- NormalizeData(myeloid, normalization.method = "LogNormalize", scale.factor = 1e4)
+myeloid <- FindVariableFeatures(myeloid, selection.method = 'vst', nfeatures = 2000)
+myeloid <- ScaleData(myeloid, vars.to.regress = c("percent.mt"), features = rownames(myeloid))
+myeloid <- RunPCA(myeloid, features = VariableFeatures(object = myeloid))
